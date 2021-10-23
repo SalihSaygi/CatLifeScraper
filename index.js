b124e4099b4ce77a23c8a25bab55ddd5a9f3c98c
Salih Saygi <salih.saygie@gmail.com>
2021-10-22 19:59:05 -0700

    Converted it to await async style to use the script as a function

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-let eventsData = [];
-
-async function getPhoto(uri) {
-  const photo = await axios.get(
-    `https://ucmerced-cdn.presence.io/event-photos/8fd48e04-025a-4b9c-9e7b-595adaeb479c/${uri}`
-  );
+export async function getPhoto(uri) {
+  try {
+    const photo = await axios.get(
+      `https://ucmerced-cdn.presence.io/event-photos/8fd48e04-025a-4b9c-9e7b-595adaeb479c/${uri}`
+    );
+  } catch (err) {
+    return err;
+  }
   return photo;
 }
 
@@ -13,47 +15,40 @@ function tagRemover(description) {
   return description.replace(/<\/?p[^>]*>/g, '');
 }
 
-axios
-  .get(
+async function getEvents() {
+  const response = await axios.get(
     'https://api.presence.io/ucmerced/v1/organizations/events/association-for-computing-machinery-uc-merced'
-  )
-  .then(function (response) {
-    // handle success
-    Promise.all(
-      response.data.map(async event => {
-        console.log(event.uri, 'uri');
-        let res;
-        try {
-          res = await axios.get(
-            `https://api.presence.io/ucmerced/v1/events/${event.uri}`
-          );
-        } catch (err) {
-          return err;
-        }
-        return res.data;
-      })
-    )
-      .then(detailedEvents => {
-        const formattedEvents = detailedEvents.map(event => {
-          const formattedEvent = {
-            apiID: event.apiId,
-            eventName: event.eventName,
-            description: tagRemover(event.description),
-            location: event.isVirtualLink ? 'Virtual' : event.location,
-            startTime: event.startDateTimeUtc,
-            endTime: event.endDateTimeUtc,
-            image: event.hasCoverImage ? event.photoUri : 'null',
-          };
-          return formattedEvent;
-        });
-        return formattedEvents;
-      })
-      .then(response => {
-        eventsData = response;
-        console.log(eventsData, 'eventsData');
-      });
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
+  );
+  const detailedEvents = await Promise.all(
+    response.data.map(async event => {
+      let detailedEvent;
+      try {
+        detailedEvent = await axios.get(
+          `https://api.presence.io/ucmerced/v1/events/${event.uri}`
+        );
+      } catch (err) {
+        return err;
+      }
+      return detailedEvent.data;
+    })
+  );
+  const formattedEvents = detailedEvents.map(event => {
+    const formattedEvent = {
+      apiID: event.apiId,
+      eventName: event.eventName,
+      description: tagRemover(event.description),
+      location: event.isVirtualLink ? 'Virtual' : event.location,
+      startTime: event.startDateTimeUtc,
+      endTime: event.endDateTimeUtc,
+      image: event.hasCoverImage ? event.photoUri : 'null',
+    };
+    return formattedEvent;
   });
+  return formattedEvents;
+}
+
+getEvents()
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => console.log(err));
